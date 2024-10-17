534a08ba89c102ab3ee91b13ece52820d952fc7b
pecor <251609@edu.p.lodz.pl>
2024-10-17 15:52:40 +0200

    nauka

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './styles/Content.css';
 
+interface TextBoxProps {
+  title: string;
+  paragraph: string;
+}
+
+const TextBox: React.FC<TextBoxProps> = ({ title, paragraph }) => {
+  return (
+    <div className="text-box">
+      <h1>{title}</h1>
+      <p>{paragraph}</p>
+    </div>
+  );
+};
+
 const Content: React.FC = () => {
-    return (
-        <div className="content-container">
-            <p>Alfiarze to wiodaca firma w Polsce zajmujaca sie kradzieza i rozporawdzaniem narkotykow w srod mlodziezy</p>
-        </div>
-    );
+  return (
+    <div className="content-container">
+      <TextBox title="ALFA.PL" paragraph="Alfiarze to wiodaca firma w Polsce zajmujaca sie kradzieza i rozporawdzaniem narkotykow w srod mlodziezy" />
+      <TextBox title="Another Title" paragraph="Second paragraph text goes here" />
+      <TextBox title="Third Title" paragraph="Third paragraph text goes here" />
+      <TextBox title="Fourth Title" paragraph="Fourth paragraph text goes here" />
+      <TextBox title="Fifth Title" paragraph="Fifth paragraph text goes here" />
+    </div>
+  );
 };
 
 export default Content;
